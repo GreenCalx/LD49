fc0e3ba632d0f6ca9408997c97d2c23207916470
GreenCalx <romain.schoenberger@gmail.com>
2021-10-02 12:57:06 +0200

    branching scenes to maingameloop

---
+++ WONKERZ/Assets/Scripts/Constants.cs
@@ -0,0 +1,12 @@
+
+public struct Constants
+{
+  // -- Scene names --
+    public const string SN_SPLASH="SchnibbleSplash";
+    public const string SN_TITLE="titlescene";
+    public const string SN_MAINGAME="main";
+
+    // -- ... --
+
+
+}
--- WONKERZ/Assets/Scripts/Scenes/TitleScreen.cs
+++ WONKERZ/Assets/Scripts/Scenes/TitleScreen.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SplashScreen : MonoBehaviour
+public class TitleScreen : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,8 @@ public class SplashScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool key_pressed = Input.anyKeyDown;
+        if (key_pressed)
+            SceneManager.LoadScene(Constants.SN_MAINGAME, LoadSceneMode.Single);     
     }
 }
