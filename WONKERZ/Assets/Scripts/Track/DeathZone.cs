50bcf81156a043d47aa161c802850c26367b13cb
GreenCalx <romain.schoenberger@gmail.com>
2021-10-03 03:46:19 +0200

    deathzones

---
+++ WONKERZ/Assets/Scripts/Track/DeathZone.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    public CheckPointManager checkPointManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider iCol)
+    {
+        if (iCol.GetComponent<CarController>())
+        {
+            checkPointManager.loadLastCP();
+        }
+    }
+}
