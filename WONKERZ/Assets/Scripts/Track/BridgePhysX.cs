74136687460659f5773d4dea579aa7e803859072
Toffanim <marc.toffanin@gmail.com>
2021-10-04 14:34:01 +0200

    Save bridge wip tweaking

--- WONKERZ/Assets/Scripts/Track/BridgePhysX.cs
+++ WONKERZ/Assets/Scripts/Track/BridgePhysX.cs
@@ -4,7 +4,7 @@ public class BridgePhysX : MonoBehaviour
 {
     public float OscFrequency = 1;
     public float OscStrength = 30;
-    public float TransStrength = 10;
+    public Vector2 TransStrength;
     private Vector3 StartRot;
     private Vector3 StartTransf;
     private float CurrentTime = 0;
@@ -21,13 +21,13 @@ public class BridgePhysX : MonoBehaviour
         CurrentTime += Time.deltaTime;
 
         var Lerp = Mathf.Sin(CurrentTime / OscFrequency);
-        var HalfLerp = Mathf.Sin(CurrentTime / (OscFrequency / 2));
+        var HalfLerp = Mathf.Cos(CurrentTime / (OscFrequency / 2));
 
         var Rot = new Quaternion();
         Rot.eulerAngles = new Vector3(StartRot.x + Lerp * OscStrength, StartRot.y, StartRot.z);
         transform.rotation = Rot;
 
-        var Tran = new Vector3(StartTransf.x, StartTransf.y, StartTransf.z - Lerp * TransStrength);
+        var Tran = new Vector3(StartTransf.x, StartTransf.y + HalfLerp * TransStrength.x, StartTransf.z - Lerp * TransStrength.y);
         transform.position = Tran;
 
     }
