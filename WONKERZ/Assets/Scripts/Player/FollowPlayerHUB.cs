ae58b1d0b2686ccc92b4d9011cd3c1d198b58066
GreenCalx <romain.schoenberger@gmail.com>
2021-11-11 11:52:00 +0100

    rockyzone in hub

--- WONKERZ/Assets/Scripts/Player/FollowPlayerHUB.cs
+++ WONKERZ/Assets/Scripts/Player/FollowPlayerHUB.cs
@@ -4,47 +4,30 @@ using UnityEngine;
 
 public class FollowPlayerHUB : MonoBehaviour
 {
-    public GameObject Following;
-    public Vector3 Distance;
-    public float LerpMult;
-    private bool Active = true;
+    // camera will follow this object
+    public Transform Target;
+    //camera transform
+    public Transform camTransform;
+    // offset between camera and target
+    public Vector3 Offset;
+    // change this value to get desired smoothness
+    public float SmoothTime = 0.3f;
 
-    public float turnSpeed = 5.0f;
-    public CheckPointManager Mng;
+    // This value will change at the runtime depending on target movement. Initialize with zero vector.
+    private Vector3 velocity = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-
+        Offset = camTransform.position - Target.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        if (!Active) return;
+        // update position
+        Vector3 targetPosition = Target.position + Offset;
+        camTransform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
 
-        var FinalPosition = Following.transform.position + Distance.x * Vector3.right + Distance.y * Vector3.up + Distance.z * Vector3.forward;
-        var FinalPositionDirection = Following.transform.position - FinalPosition;
-        var MaxDistancePosition = (FinalPosition - (FinalPositionDirection * LerpMult));
-
-        var MaxDistanceMagn = (MaxDistancePosition - FinalPosition).magnitude;
-
-        var CurrentDistance = (transform.position - FinalPosition).magnitude;
-
-        var Lerp = (CurrentDistance / MaxDistanceMagn);
-
-        transform.position = Vector3.Lerp(transform.position, FinalPosition, Lerp);
-        transform.LookAt(Following.transform.position);
-
-        var xz_cam = new Vector3( transform.forward.x, 0, transform.forward.z);
-        var xz_target = new Vector3( Following.transform.forward.x, 0, Following.transform.forward.z);
-        var xz_angle = Vector3.Angle( xz_cam, xz_target);
-
-        if ( xz_angle >= 0 )
-        {
-            transform.RotateAround(Following.transform.position, Vector3.up, xz_angle );
-        }
-
-        
+        // update rotation
+        transform.LookAt(Target);
     }
 }
