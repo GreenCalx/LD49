703d86f69ccd8e4b81786720be7f17b2061028df
BlueCalx <romain.schoenberger@gmail.com>
2022-09-05 00:04:10 +0200

    TRACK | Desert track update + CheckpointV2 + Fonts

--- WONKERZ/Assets/Scripts/Track/CheckPoint.cs
+++ WONKERZ/Assets/Scripts/Track/CheckPoint.cs
@@ -9,17 +9,27 @@ public class CheckPoint : AbstractCameraPoint
     private float CurrentAnimationLength;
     private bool IsAnimating = false;
 
+    [Header("MAND")]
     public Transform respawn_location;
+    public Animator animator;
+    public ParticleSystem particles_blend;
+    public ParticleSystem particles_add;
+
     [HideInInspector]
     public CheckPointManager cpm;
     public ParticleSystemForceField activation_pff;
     public ParticleSystemForceField base_pff;
+
+
     // Start is called before the first frame update
     void Start()
     {
         //hasManager = subscribeToManager();
         activation_pff.gameObject.SetActive(false);
         base_pff.gameObject.SetActive(true);
+        if (animator==null)
+            animator = GetComponentInChildren<Animator>();
+        animator.SetBool("TRIGGERED", false);
     }
 
 
@@ -57,9 +67,15 @@ public class CheckPoint : AbstractCameraPoint
         CarController player = iCol.GetComponent<CarController>();
         if (!!player)
         {
+            animator.SetBool("TRIGGERED", true);
             cpm.notifyCP(this.gameObject);
             activation_pff.gameObject.SetActive(true);
             base_pff.gameObject.SetActive(false);
+
+            if (!!particles_add)
+                particles_add.Stop();
+            if (!!particles_blend)
+                particles_blend.Stop();
 
             Cam = Access.CameraManager().active_camera.GetComponent<Camera>();
             StartCameraAnimation();
