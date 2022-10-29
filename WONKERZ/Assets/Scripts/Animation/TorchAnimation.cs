bde37dfb918c97c8b3842e0f71574913679f2c39
BlueCalx*MainPC <romain.schoenberger@gmail.com>
2022-10-29 02:58:38 +0200

    TRACK | Light tweaks attempt V1 in desert track

---
+++ WONKERZ/Assets/Scripts/Animation/TorchAnimation.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchAnimation : MonoBehaviour
+{
+    public AnimationCurve AC;
+    public Light target_light;
+    private float elapsed_time;
+    private float base_intensity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsed_time = 0f;
+        base_intensity = target_light.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed_time += Time.deltaTime; // TODO : factorize in a scene global timer ?
+        updateIntensity();
+    }
+
+    private void updateIntensity()
+    {
+        float val = AC.Evaluate(elapsed_time);
+        target_light.intensity = base_intensity * val;
+    }
+}
