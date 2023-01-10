7dd2bbe1f83f35c707bb50e8ed34743d93d65459
BlueCalx*MainPC <romain.schoenberger@gmail.com>
2023-01-10 14:58:58 +0100

    QoL | Savestates now have extra slot than can be pre-set in the inspector

--- WONKERZ/Assets/Scripts/Test/TSTSaveStates.cs
+++ WONKERZ/Assets/Scripts/Test/TSTSaveStates.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TSTSaveStates : MonoBehaviour
 {
@@ -12,6 +13,14 @@ public class TSTSaveStates : MonoBehaviour
     public KeyCode load;
     public KeyCode save;
 
+    [Serializable]
+    public struct ESS
+    {
+        public KeyCode k;
+        public Transform t;
+    }
+    public List<ESS> ess;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,25 +30,9 @@ public class TSTSaveStates : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(load)) // load
+        if (Input.GetKeyDown(load) || pollExtraSaveStates()) // load
         {
-            if (!hasSS)
-            {
-                Debug.LogError("No save state to load. Loading start portal.");
-                ss_pos = startPortal.position;
-                ss_rot = Quaternion.identity;
-                hasSS = true;
-            }
-            Rigidbody rb2d = Access.Player().gameObject.GetComponentInChildren<Rigidbody>();
-            if (!!rb2d)
-            {
-                rb2d.velocity           = Vector3.zero;
-                rb2d.angularVelocity    = Vector3.zero;
-            }
-            Access.Player().gameObject.transform.position = ss_pos;
-            Access.Player().gameObject.transform.rotation = ss_rot;
-
-            Debug.Log("Savestate loaded");
+            loadState();
         } else if (Input.GetKeyDown(save))
         {
             ss_pos = Access.Player().gameObject.transform.position;
@@ -47,5 +40,48 @@ public class TSTSaveStates : MonoBehaviour
             hasSS = true;
             Debug.Log("Savestate updated");
         }
+    }
+
+    public bool pollExtraSaveStates()
+    {
+        if (ess == null)
+            return false;
+
+        foreach (ESS e in ess)
+        {
+            if (Input.GetKeyDown(e.k))
+            {
+                Transform t = e.t;
+                if (t==null)
+                {
+                    Debug.LogError("No transform could be found for given KeyCode : " + e.k);
+                    return false;
+                }
+                ss_pos = t.position;
+                ss_rot = t.rotation;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void loadState()
+    {
+        if (!hasSS)
+        {
+            Debug.LogError("No save state to load. Loading start portal.");
+            ss_pos = startPortal.position;
+            ss_rot = Quaternion.identity;
+            hasSS = true;
+        }
+        Rigidbody rb2d = Access.Player().gameObject.GetComponentInChildren<Rigidbody>();
+        if (!!rb2d)
+        {
+            rb2d.velocity           = Vector3.zero;
+            rb2d.angularVelocity    = Vector3.zero;
+        }
+        Access.Player().gameObject.transform.position = ss_pos;
+        Access.Player().gameObject.transform.rotation = ss_rot;
+        Debug.Log("Savestate loaded");
     }
 }
