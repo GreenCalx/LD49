ef533397330e060a9eee7866a8eb365e7443de38
GreenCalx <romain.schoenberger@gmail.com>
2023-03-01 17:10:09 +0100

    TRACK | Gas Station update : smoke on activation + colored lightpost indicator
    + New layer : ColliderWithParticles
    + Cameras culling mask update

--- WONKERZ/Assets/Scripts/Track/GasStation.cs
+++ WONKERZ/Assets/Scripts/Track/GasStation.cs
@@ -4,12 +4,18 @@ public class GasStation : MonoBehaviour
 {
     public Animator animator;
 
+    public Color LightColorOnDeactivated;
+    public Color LightColorOnActivated;
+    public Light CPAnimatorLight;
+    public Light CPAnimatorHalo;
+
     public float nutConversionInterval = 0.2f;
     private float nutConversionElapsed = 99f;
 
     private bool IsPumpingGas = false;
     private string animatorParm = "IsPumping";
     private string animatorStationActivationParm = "IsStationActivated";
+    private string animatorActivationState = "ActivateStation";
 
     public bool bypassNutsCost = false;
 
@@ -17,6 +23,7 @@ public class GasStation : MonoBehaviour
     void Start()
     {
         nutConversionElapsed = 99f;
+        changeLightsColor(LightColorOnDeactivated);
     }
 
     // Update is called once per frame
@@ -25,12 +32,21 @@ public class GasStation : MonoBehaviour
 
     }
 
+    void changeLightsColor(Color iColor)
+    {
+        if (CPAnimatorLight!=null)
+        { CPAnimatorLight.color = iColor; }
+        if (CPAnimatorHalo!=null)
+        { CPAnimatorHalo.color = iColor; }
+    }
+
     void OnTriggerEnter(Collider iCollider)
     {
         if (Utils.isPlayer(iCollider.gameObject))
         {
             Access.CheckPointManager().notifyGasStation(this);
             animator.SetBool(animatorStationActivationParm, true);
+            changeLightsColor(LightColorOnActivated);
         }
     }
 
