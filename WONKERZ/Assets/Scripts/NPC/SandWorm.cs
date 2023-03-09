84eeaa978e0222aace44eaa06aed7e897a4026aa
GreenCalx <romain.schoenberger@gmail.com>
2023-03-09 21:17:42 +0100

    TRACK | Sandworm ground detector + Particle tweak
    + renamed a Tag to OnlyCollideWithDefault

--- WONKERZ/Assets/Scripts/NPC/SandWorm.cs
+++ WONKERZ/Assets/Scripts/NPC/SandWorm.cs
@@ -7,6 +7,11 @@ public class SandWorm : MonoBehaviour
 {
     public float wanderRadius;
     public float wanderTimer;
+
+    public ParticleSystem PS_Front;
+    public ParticleSystem PS_Back;
+    public GroundDetector FrontDetector;
+    public GroundDetector BackDetector;
  
     private NavMeshAgent agent;
     private float timer;
@@ -28,6 +33,22 @@ public class SandWorm : MonoBehaviour
             Debug.DrawRay(newPos, Vector3.up, Color.red);
             agent.SetDestination(newPos);
             timer = 0;
+        }
+        
+        if (FrontDetector.crossedGround)
+        {
+            if (!PS_Front.isPlaying)
+            { PS_Front.Play(); }
+        } else {
+            PS_Front.Stop();
+        }
+
+        if (BackDetector.crossedGround)
+        {
+            if (!PS_Back.isPlaying)
+            { PS_Back.Play(); }
+        } else {
+            PS_Back.Stop();
         }
     }
 
