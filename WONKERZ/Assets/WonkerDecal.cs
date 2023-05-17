79b2b422316f800eba7b4fb330d690fc14cbd747
GreenCalx <romain.schoenberger@gmail.com>
2023-05-17 12:02:55 +0200

    [GFX][Decal] Adding time based animated decal v0

---
+++ WONKERZ/Assets/WonkerDecal.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Schnibble;
+
+
+public class WonkerDecal : MonoBehaviour
+{
+
+    public DecalRenderer decalRenderer;
+    public CarController cont;
+
+    void Start()
+    {
+        if (decalRenderer.cam==null)
+        {
+            decalRenderer.SetCamera(Access.CameraManager().active_camera.GetComponent<Camera>());
+        }
+    }
+
+    public void SetAnimationTime(float t)
+    {
+        decalRenderer.mat.SetFloat("_AnimationTime", t);             
+    }
+
+    void Update()
+    {
+        if (decalRenderer.cam!=Access.CameraManager().active_camera.GetComponent<Camera>())
+        {
+            this.Log("cam change on decal");
+            decalRenderer.SetCamera(Access.CameraManager().active_camera.GetComponent<Camera>());
+        }
+
+    }
+}
