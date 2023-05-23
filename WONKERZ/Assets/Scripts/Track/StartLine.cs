b3cf1d848b97a6cacd8758983a79ad398291d68b
GreenCalx <romain.schoenberger@gmail.com>
2023-05-24 01:36:57 +0200

    TRACK | Start Line added to desert + updated on other tracks
    + SFX
    + TransparentFromPalette material

--- WONKERZ/Assets/Scripts/Track/StartLine.cs
+++ WONKERZ/Assets/Scripts/Track/StartLine.cs
@@ -6,13 +6,35 @@ using Schnibble;
 
 public class StartLine : MonoBehaviour
 {
-    //[Header("MAND")]
+    [Header("MAND")]
     //public string track_name;
+    public GameObject UIStartTrackRef;
+    public AudioSource  startLineCrossed_SFX;
+    public bool destroyOnActivation = true;
+    public float uiShowTime = 2f;
 
+    private GameObject UIStartTrackInst = null;
+
+
+    private float uiVisibleElapsed = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiVisibleElapsed = 0f;
+    }
+
+    void Update()
+    {
+        if (UIStartTrackInst != null)
+        {
+            uiVisibleElapsed += Time.deltaTime;
+            if (uiVisibleElapsed >= uiShowTime)
+            {
+                Destroy(UIStartTrackInst.gameObject);
+                if (destroyOnActivation)
+                    Destroy(gameObject);
+            }
+        }
     }
 
     void OnTriggerEnter(Collider iCol)
@@ -21,9 +43,22 @@ public class StartLine : MonoBehaviour
 
         if (!!cc)
         {
+            if (UIStartTrackInst==null)
+            {
+                UIStartTrackInst = Instantiate(UIStartTrackRef);
+                uiVisibleElapsed = 0f;
+            }
+
+            if (!!startLineCrossed_SFX)
+            {
+                startLineCrossed_SFX.Play();
+            }
+            
+
             // start line crossed !! gogogo
             Scene currentScene = SceneManager.GetActiveScene();
             Access.TrackManager().launchTrack(currentScene.name);
+
         }
     }
 }
