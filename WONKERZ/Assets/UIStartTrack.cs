bff914527795ab9f92868fd5be8d6592f700f6c1
GreenCalx <romain.schoenberger@gmail.com>
2023-07-13 12:56:12 +0200

    TRACK | Start UI countdown small anim + firepit in tower ground level

--- WONKERZ/Assets/UIStartTrack.cs
+++ WONKERZ/Assets/UIStartTrack.cs
@@ -6,14 +6,45 @@ public class UIStartTrack : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI countdown_txt;
 
+    public Texture2D opacityRamp;
+    public AnimationCurve fontSizeRatioOverTime;
+
+
+    private float elapsedTime = 0f;
+    private float initFontSize = 12f;
+    private string currString = "";
+
+    void Start()
+    {
+        elapsedTime = 0f;
+        initFontSize = countdown_txt.fontSize;
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.fixedDeltaTime;
+
+        float curr_time = Mathf.Min(elapsedTime, 1f);
+
+        countdown_txt.fontSize = fontSizeRatioOverTime.Evaluate(curr_time) * initFontSize;
+
+        float width = opacityRamp.width;
+        Color newcolor = opacityRamp.GetPixel((int)(curr_time*width),0);
+        Debug.Log("alpha : " + newcolor.a);
+        countdown_txt.alpha = newcolor.a;
+    }
+
+
     // Update is called once per frame
     public void updateDisplay(float iCurrCountdownValue)
     {
+        if (iCurrCountdownValue < 1f)      {currString = "3";}
+        if ((iCurrCountdownValue >= 1f))   {currString = "2";}
+        if ((iCurrCountdownValue >= 2f))   {currString = "1";}
+        if ((iCurrCountdownValue >= 3f))   {currString = "GO!"; }
 
-        if (iCurrCountdownValue < 1f)      {countdown_txt.text = "3";}
-        if ((iCurrCountdownValue >= 1f))   {countdown_txt.text = "2";}
-        if ((iCurrCountdownValue >= 2f))   {countdown_txt.text = "1";}
-        if ((iCurrCountdownValue >= 3f))   {countdown_txt.text = "GO!";}
+        if (currString!=countdown_txt.text)
+        { countdown_txt.text = currString; elapsedTime = 0f; }
     }
 
 
