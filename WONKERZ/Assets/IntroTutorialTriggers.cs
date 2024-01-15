e9816d8c8a1af4df477bd6622098b6461b2c570d
GreenCalx <romain.schoenberger@gmail.com>
2024-01-15 18:40:21 +0100

    INTRO | Candles, prophecy wall, more deco, tutorial stop menu triggers to get back up & nuts

--- WONKERZ/Assets/IntroTutorialTriggers.cs
+++ WONKERZ/Assets/IntroTutorialTriggers.cs
@@ -11,18 +11,51 @@ public class IntroTutorialTriggers : MonoBehaviour
     public bool weightTutoDone = false;
     public UITutorialStopWindow weightTutoWindow;
 
+    public bool getUpTutoDone = false;
+    public UITutorialStopWindow getUpTutoWindow;
+    public float timeThrStuckDetection = 1f;
+
     private CollectiblesManager CM;
+    private PlayerController PC;
+
+    private float elapsedTimeStuckDetection = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         CM = Access.CollectiblesManager();
+        PC = Access.Player();
+
+        elapsedTimeStuckDetection = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         pollFirstNut();
+        pollPlayerStuck();
+    }
+
+    public void pollPlayerStuck()
+    {
+        if (getUpTutoDone)
+            return;
+        
+        bool playerIsActive = PC.IsInMenu();
+        bool playerIsGrounded = PC.TouchGroundAll();
+        bool playerHasLowSpeed = PC.car.GetCurrentSpeed() < 5f;
+
+        if (!playerIsActive && !playerIsGrounded && playerHasLowSpeed)
+        {
+            elapsedTimeStuckDetection += Time.deltaTime;
+            if (elapsedTimeStuckDetection >= timeThrStuckDetection)
+            {
+                Instantiate(getUpTutoWindow);
+                getUpTutoDone = true; 
+            }
+        } else {
+            elapsedTimeStuckDetection = 0f;
+        }
     }
 
     public void pollFirstNut()
