a552de7b3f5e0a42851b27aed60d71e63fba0c6c
Toffanim <marc.toffanin@gmail.com>
2024-03-25 16:48:36 +0100

    [Perf][GUI] Cache version string in buildinfos.

--- WONKERZ/Assets/2_Scripts/Utils/buildinfos.cs
+++ WONKERZ/Assets/2_Scripts/Utils/buildinfos.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class buildinfos : MonoBehaviour
 {
+    string version = "Build version : " + Constants.MajorVersion + "." + Constants.MinorVersion;
     // Start is called before the first frame update
     void OnGUI()
     {
 	    GUI.Label(new Rect(0, 0, 100, 100), ((int)(1.0f / Time.unscaledDeltaTime)).ToString());
-        GUI.Label(new Rect(0, 20, 200, 100), "Build version : " + Constants.MajorVersion + "." + Constants.MinorVersion);
+        GUI.Label(new Rect(0, 20, 200, 100), version);
         GUI.Label(new Rect(0, 40, 200, 100), "Time since startup : " + Time.realtimeSinceStartup);
     }
 }
