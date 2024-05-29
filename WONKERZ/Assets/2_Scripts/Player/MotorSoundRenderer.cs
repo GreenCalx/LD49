f713eeaf6b2e9232d386dcfa450e56154efe791a
Toffanim <marc.toffanin@gmail.com>
2024-05-29 21:46:51 +0200

    [PhysX] New new, new new, last, new, final, new last final car v1.0.0

--- WONKERZ/Assets/2_Scripts/Player/MotorSoundRenderer.cs
+++ WONKERZ/Assets/2_Scripts/Player/MotorSoundRenderer.cs
@@ -4,9 +4,26 @@ using Schnibble;
 public class MotorSoundRenderer : MonoBehaviour
 {
     public SchCar car;
+
+    public float minPitch = 0.8f;
+    public float maxPitch = 2.0f;
+    public float ratioMul = 1.0f;
+
+    AudioSource source;
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
     // Update is called once per frame
     void Update()
     {
-        GetComponent<AudioSource>().pitch = 0.8f + 1 / ((100) / ((float)car.GetCurrentSpeedInKmH()));
+        // old way
+        //GetComponent<AudioSource>().pitch = 0.8f + 1 / ((100) / ((float)car.GetCurrentSpeedInKmH()));
+
+        //new way
+        var maxRPM = (float)car.engine.maxRPM;
+        var currentRPM = (float)car.engine.rpm;
+        var ratio = currentRPM / maxRPM;
+        source.pitch = Mathf.Clamp(minPitch + ratioMul * (1 - (1 - ratio)*(1-ratio)), minPitch, maxPitch);
     }
 }
