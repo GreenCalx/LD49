f5f6e0b16d1230dcdc56f241adc27b27be867234
Toffanim <marc.toffanin@gmail.com>
2024-05-29 23:01:01 +0200

    [Car] Adding possibility for a little of rotation for the weight indicator.

--- WONKERZ/Assets/2_Scripts/Player/CarWeightIndicator.cs
+++ WONKERZ/Assets/2_Scripts/Player/CarWeightIndicator.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class CarWeightIndicator : MonoBehaviour
 {
-    public GameObject carCOM;
+    public Rigidbody body;
     public GameObject indicator;
-    public float indicatorHeight;
+    public Vector3 offsetLocal;
+    public bool useRotation = false;
     // Update is called once per frame
     void Update()
     {
-        indicator.transform.position = carCOM.transform.TransformPoint(Vector3.Scale(carCOM.transform.localPosition, new Vector3(1,0,1)) + indicatorHeight * Vector3.up) ;
+        var com = body.worldCenterOfMass;
+        indicator.transform.position = com + body.transform.TransformVector(offsetLocal);
+        if (useRotation)
+        {
+            // try to add a lil bit of rotation
+            // TODO: make it real! Do not assume COM was at 0, Vector3.up is the up, etc...
+            var diff = body.transform.TransformVector(body.centerOfMass + offsetLocal).normalized;
+            indicator.transform.rotation = Quaternion.LookRotation(body.transform.forward, diff);
+        }
     }
 }
