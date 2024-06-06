2071c9f4511fdb23929fd79eba67bc3f3e9c7158
Toffanim <marc.toffanin@gmail.com>
2024-06-06 03:11:17 +0200

    Fix pendulum trap.

--- WONKERZ/Assets/2_Scripts/Track/Traps/PendulumTrap.cs
+++ WONKERZ/Assets/2_Scripts/Track/Traps/PendulumTrap.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class PendulumTrap : MonoBehaviour
 {
+    HingeJoint joint;
+    Rigidbody body;
+    public float angleLimit     = 45.0f;
+    public float targetVelocity = 10.0f;
+    public float minTargetVelocity = 10.0f;
 
-    public Vector3 axis;
+
+    #if false
+    Vector3 axis;
     [Range(0.01f, 2f)]
     public float amplitude = 1f;
     [Range(0.01f, 4f)]
@@ -16,8 +23,28 @@ public class PendulumTrap : MonoBehaviour
     public float timerForPhaseSwitch = 1f;    
     
     float timer = 0f;
-    
+    #endif
 
+    void Awake() {
+        joint = GetComponent<HingeJoint>();
+        body = GetComponent<Rigidbody>();
+
+        var motor = joint.motor;
+        motor.targetVelocity = targetVelocity;
+        joint.motor = motor;
+    }
+
+    void FixedUpdate() {
+        var angle = joint.angle;
+        var motor = joint.motor;
+        if (angle < -angleLimit || angle > angleLimit) targetVelocity = -targetVelocity;
+
+        motor.targetVelocity = Mathf.Lerp(minTargetVelocity * Mathf.Sign(targetVelocity), targetVelocity, (angleLimit - Mathf.Abs(angle)) / angleLimit);
+
+        joint.motor = motor;
+    }
+
+    #if false
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -36,7 +63,7 @@ public class PendulumTrap : MonoBehaviour
                     axis.x * (speed * (1-timer))*amplitude, 
                     axis.y * (speed * (1-timer))*amplitude, 
                     axis.z * (speed * (1-timer))*amplitude
-                    );
+                );
                 break;
             case 1:
                 transform.Rotate(
@@ -55,8 +82,9 @@ public class PendulumTrap : MonoBehaviour
                     axis.x * (speed * timer)*amplitude, 
                     axis.y * (speed * timer)*amplitude, 
                     axis.z * (speed * timer)*amplitude
-                    );
+                );
                 break;
         }
     }
+    #endif
 }
