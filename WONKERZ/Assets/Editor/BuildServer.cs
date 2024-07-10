529067a85c225ab218780a1d5620c7a091e7a127
Toffanim <marc.toffanin@gmail.com>
2024-07-10 14:54:35 +0200

    [Online] Adding Unix server build.

--- WONKERZ/Assets/Editor/BuildServer.cs
+++ WONKERZ/Assets/Editor/BuildServer.cs
@@ -1,9 +1,9 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEditor.Build.Reporting;
+using System.IO;
 
 // Output the build size or a failure depending on BuildPlayer.
-
 public class Builds 
 {
     public struct SavedSettings {
@@ -25,20 +25,49 @@ public class Builds
         EditorUserBuildSettings.standaloneBuildSubtarget = save.subTarget;
     }
 
+    public static void CreatePathIfNotExists(string path) {
+        var dataPath = Application.dataPath;
+        var projectRootFolder = Path.GetDirectoryName(dataPath);
+        Debug.Log(projectRootFolder);
+        var desiredPath = Path.Combine(projectRootFolder, Path.GetDirectoryName(path));
+        if (!Directory.Exists(desiredPath)) {
+            Directory.CreateDirectory(desiredPath);
+        }
+    }
 
-    [MenuItem("Build/BuildServerWin")]
-    public static void BuildServerWin()
-    {
+    public static readonly string exeName = "SchLobbyServer";
+    public static readonly string scenePath = "Assets/0_Scenes/Online/ServerScene.unity";
+    public static void BuildServerPlateform(BuildTarget plateform) {
         var savedSettings = Builds.SaveSettings();
 
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
 
-        buildPlayerOptions.scenes = new[] { "Assets/0_Scenes/Online/ServerScene.unity" };
+        buildPlayerOptions.scenes = new[] { Builds.scenePath };
 
-        buildPlayerOptions.locationPathName = "Build/Online/Server/Win64";
+        buildPlayerOptions.target = plateform;
+        buildPlayerOptions.subtarget        = (int)StandaloneBuildSubtarget.Server;
 
-        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
-        buildPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Server;
+        string platformName = "";
+        string platformExeExtension = "";
+        switch (plateform) {
+            case BuildTarget.StandaloneLinux64:
+                platformName = "Unix64";
+                platformExeExtension = "out";
+                break;
+            case BuildTarget.StandaloneWindows64:
+                platformName = "Win64";
+                platformExeExtension = "exe";
+                break;
+            default:
+                Debug.LogError("Plateform unknowkn.");
+                RestoreSettings(savedSettings);
+                return;
+        };
+
+        var path = "Build/" + platformName + "/Online/Server/" + Builds.exeName + "." + platformExeExtension;
+        Builds.CreatePathIfNotExists(path);
+        buildPlayerOptions.locationPathName = path;
+        Debug.Log(path);
 
         buildPlayerOptions.extraScriptingDefines = new string[] { "SCHSERVER" };
 
@@ -61,5 +90,17 @@ public class Builds
         }
 
         RestoreSettings(savedSettings);
+    }
+
+    [MenuItem("Build/BuildServerWin")]
+    public static void BuildServerWin()
+    {
+        Builds.BuildServerPlateform(BuildTarget.StandaloneWindows64);
+    }
+
+    [MenuItem("Build/BuildServerUnix")]
+    public static void BuildServerLinux()
+    {
+        Builds.BuildServerPlateform(BuildTarget.StandaloneLinux64);
     }
 }
