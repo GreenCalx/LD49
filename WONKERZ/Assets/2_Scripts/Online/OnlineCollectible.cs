GreenCalx/LD49

commit 2a67728635b42eb0570e9af80d6e69cbb9b16f63

[CleanUp] Add comments, remove unused code, make online code clearer.

WONKERZ/Assets/2_Scripts/Online/OnlineGameManager.cs
@@ -93,22 +93,26 @@ public class OnlineGameManager : NetworkBehaviour
 
     public class GameSettings
     {
-        public uint countdownDuration; // in seconds
-        public uint gameDuration = 180; // in Seconds
-        public uint postGameDuration = 30;
+        public uint countdownDuration  = 3; // in seconds
+        public uint gameDuration       = 180; // in Seconds
+        public uint postGameDuration   = 30;
         public uint trackEventTimeStep = 60;
-        public List<string> trialPool = new List<string>() { "RaceTrial01", "MountainTrial01" };
-        public string selectedTrial = "RaceTrial01";
+        public List<string> trialPool  = new List<string>() {
+            "RaceTrial01",
+            "MountainTrial01"
+        };
+        public string selectedTrial    = "RaceTrial01";
     };
     public GameSettings settings;
 
     [Header("INTERNALS")]
     public readonly SyncList<OnlinePlayerController> uniquePlayers = new SyncList<OnlinePlayerController>();
+
     public OnlinePlayerController localPlayer;
 
-    public int expectedPlayersFromLobby;
-
     public bool waitForPlayersToBeReady = false;
+    bool allPlayersSpawned = false;
+    bool allPlayersLoaded  = false;
 
     [SyncVar]
     public float countdownElapsed = 0f;
@@ -126,45 +130,40 @@ public class OnlineGameManager : NetworkBehaviour
 
     public Action<bool> onShowUITrackTime;
 
-
-    /* ----------------------------------
-    Server
-    ------------------------------------ */
+    #region Server
 
     public override void OnStartServer()
     {
         this.Log("Start onlineGameManager : server.");
-        // TODO: remove this as we are already kinda a singleton ourselves.
-        expectedPlayersFromLobby = NetworkRoomManagerExt.singleton.roomSlots.Count;
-        NetworkRoomManagerExt.singleton.onlineGameManager = this;
-
         StartCoroutine(ServerRoutine());
     }
 
     [Server]
     IEnumerator ServerRoutine()
     {
-        yield return StartCoroutine(WaitForAllClientToSpawn    ());
-        yield return StartCoroutine(WaitForAllPlayersToBeLoaded());
-        // All players have been loaded inside the OnlineGameRoom,
-        // now we load the track.
-        NetworkRoomManagerExt.singleton.loadOpenCourse();
-        // wait for the track to be loaded.
-        while(!NetworkRoomManagerExt.singleton.subsceneLoaded) yield return null;
-        // do stuff
+        NetworkRoomManagerExt manager = NetworkRoomManagerExt.singleton;
+
+        // NOTE: why do we need this bool?
         Access.GameSettings().isOnline = true;
 
+        yield return StartCoroutine(WaitForAllClientToSpawn    ());
+        yield return StartCoroutine(WaitForAllPlayersToBeLoaded());
         RpcAllPlayersLoaded();
         FreezeAllPlayers(true);
+        // All players have been loaded inside the OnlineGameRoom,
+        // now we load the track.
+        yield return StartCoroutine(LoadScene(Constants.SN_OPENCOURSE));
+        // TODO: should we wait for all players to load the scene?
+        // arg for => we are sure that everyone has the same state
+        // arg against => one player might have a very slow PC and slow everyone dawn or make the server crash.
 
-        // Show the scene.
+        // Finally show the scene.
         Access.SceneLoader().unloadLoadingScene();
-
+        // Do we wait for players to input Start to be ready or directly start the game.
         if (waitForPlayersToBeReady)
         {
             yield return StartCoroutine(WaitForAllPlayersToBeReady());
         }
-
         RpcAllPlayersLockAndLoaded();
 
         yield return StartCoroutine(SpinTrialRoulette());
@@ -187,11 +186,15 @@ public class OnlineGameManager : NetworkBehaviour
         // TODO:
         // Add a timeout if we cannot have all players spawed for 10s or something.
         this.Log("Waiting for client to spawn.");
-        while (uniquePlayers.Count != expectedPlayersFromLobby)
+
+        allPlayersSpawned = false;
+        while (!allPlayersSpawned)
         {
             this.Log("Number of client spawned: " + uniquePlayers.Count);
+            allPlayersSpawned = uniquePlayers.Count != NetworkRoomManagerExt.singleton.roomSlots.Count;
             yield return null;
         }
+
         this.Log("AllClientSpawned." + uniquePlayers.Count);
     }
 
@@ -212,10 +215,36 @@ public class OnlineGameManager : NetworkBehaviour
         // After a client has spawned we wait for it to be "loaded" meaning every dependencies are resolved client-side.
         // This is to avoid having Rpc sent when the client is not fully loaded.
         // This should not happen, but for now it happens sometimes because Mirror does not spown the localPlayer as if it was a real NetworkServer.Spawn I guess.
-        this.Log("Waiting for clients to be loaded.");
-        while (!AreAllPlayersLoaded()) { yield return null; }
+        this.Log("WaitForAllPlayersToBeLoaded.");
+
+        allPlayersLoaded = false;
+        while (!allPlayersLoaded) {
+            allPlayersLoaded = AreAllPlayersLoaded();
+            yield return null;
+        }
+
         this.Log("AllClientLoaded.");
     }
+
+    [Server]
+    IEnumerator LoadScene(string sceneName) {
+        NetworkRoomManagerExt manager = NetworkRoomManagerExt.singleton;
+        // listen for loadOpenCourse callback;
+        bool sceneLoaded = false;
+        Action<string> callback = delegate (string scene) {
+            if (scene == Constants.SN_OPENCOURSE) {
+                sceneLoaded = true;
+            }
+        };
+
+        manager.OnRoomServerSceneChangedCB += callback;
+        manager.ServerSceneOperation(sceneName, SceneOperation.LoadAdditive);
+        while(!sceneLoaded) yield return null;
+        manager.OnRoomServerSceneChangedCB -= callback;
+
+        yield break;
+    }
+
 
     [Server]
     IEnumerator StartGame()
@@ -302,48 +331,10 @@ public class OnlineGameManager : NetworkBehaviour
             yield return null;
         }
 
-        this.Log("End post game.");
-
-        //unload
-        NetworkRoomManagerExt.singleton.unloadSelectedTrial();
         // shutdown server
         RpcDisconnectPlayers();
+
         NetworkServer.Shutdown();
-    }
-
-
-    [Server]
-    IEnumerator WaitSessions()
-    {
-        // Wait to load all scene.
-        while (!openCourseLoaded)
-        {
-            openCourseLoaded = NetworkRoomManagerExt.singleton.subsceneLoaded;
-
-            yield return null;
-        }
-
-        FreezeAllPlayers(true);
-
-        while (uniquePlayers.Count != expectedPlayersFromLobby)
-        {
-            yield return null;
-        }
-
-
-        while (!AreAllPlayersLoaded())
-        {
-            yield return null;
-        }
-
-        //RpcNotifyOfflineMgrAllPlayersLoaded();
-
-        while (!AreAllPlayersReady())
-        {
-            yield return null;
-        }
-
-        countdownElapsed = 0f;
     }
 
     [Server]
@@ -396,17 +387,9 @@ public class OnlineGameManager : NetworkBehaviour
     [Server]
     IEnumerator LoadTrialScene()
     {
-
-        // Unload open course on server and load trial
-        NetworkRoomManagerExt.singleton.unloadOpenCourse();
-
-        NetworkRoomManagerExt.singleton.selectedTrial = settings.selectedTrial;
-        NetworkRoomManagerExt.singleton.loadSelectedTrial();
-        // launch transition to trial on clients from server
-        NetworkRoomManagerExt.singleton.clientLoadSelectedTrial();
-
-        //if (isServer)
-        //RpcRefreshOfflineGameMgr();
+        var manager = NetworkRoomManagerExt.singleton;
+        manager.ServerSceneOperation(Constants.SN_OPENCOURSE, SceneOperation.UnloadAdditive);
+        manager.ServerSceneOperation(settings.selectedTrial , SceneOperation.LoadAdditive  );
 
         yield break;
     }
@@ -478,10 +461,7 @@ public class OnlineGameManager : NetworkBehaviour
     [Server]
     public bool AreAllPlayersReady()
     {
-        if (uniquePlayers.Count < expectedPlayersFromLobby)
-            return false;
-
-        foreach (var opc in uniquePlayers) if (!opc.IsReady()) return false;
+        foreach (var opc in uniquePlayers) if (!opc.isReady) return false;
 
         return true;
     }
@@ -489,17 +469,13 @@ public class OnlineGameManager : NetworkBehaviour
     [Server]
     public bool AreAllPlayersLoaded()
     {
-        if (uniquePlayers.Count < expectedPlayersFromLobby)
-            return false;
-
-        foreach (var opc in uniquePlayers) if (!opc.IsLoaded()) return false;
+        foreach (var opc in uniquePlayers) if (!opc.isLoaded) return false;
         return true;
     }
 
+    #endregion
 
-    /* ----------------------------------
-    Client
-    ------------------------------------ */
+    #region Client
 
     override public void OnStartClient()
     {
@@ -598,4 +574,6 @@ public class OnlineGameManager : NetworkBehaviour
 
         UIPlayer.showTrackTime = iState;
     }
+
+    #endregion
 }

WONKERZ/Assets/2_Scripts/Online/OnlineStartLine.cs
@@ -137,7 +137,7 @@ public class OnlineStartLine : NetworkBehaviour, IControllable
     {
         if (Entry.GetButtonState((int)PlayerInputs.InputCode.UIStart).down) {
             var localPlayer = OnlineGameManager.singleton.localPlayer;
-            var readyState = !localPlayer.IsReady();
+            var readyState = !localPlayer.isReady;
 
             UIIsReadyHandle.SetActive(readyState);
             UIReadyUpHandle.SetActive(!readyState);

WONKERZ/Assets/0_Scenes/Online/OnlineGameRoom.unity
@@ -765,10 +765,40 @@ MonoBehaviour:
   m_Script: {fileID: 11500000, guid: 643efbbb0ee1cc44a9ac2806b25db146, type: 3}
   m_Name: 
   m_EditorClassIdentifier: 
+  _uiTheme:
+    rid: -2
+  Parent: {fileID: 0}
+  onSelect:
+    m_PersistentCalls:
+      m_Calls: []
+  onDeselect:
+    m_PersistentCalls:
+      m_Calls: []
+  onActivate:
+    m_PersistentCalls:
+      m_Calls: []
+  onDeactivate:
+    m_PersistentCalls:
+      m_Calls: []
+  activator: {fileID: 0}
+  isSelected: 0
+  isActivated: 0
+  isOnEnableCallingActivate: 1
+  isOnDisableCallingDeactivate: 1
+  useParentInputManager: 1
+  inputMgr: {fileID: 0}
+  isTakingInputPrecedence: 1
+  useParentInputs: 0
+  inputActivate: UIActivate
   prefab_UIPostGameLine: {fileID: 7188235053611523014, guid: 261d43b511d5fec42a1fc9577b3c5d4a, type: 3}
   timeLbl: {fileID: 365368352}
   postGameLines_Handle: {fileID: 481244938}
   OPC: {fileID: 0}
+  references:
+    version: 2
+    RefIds:
+    - rid: -2
+      type: {class: , ns: , asm: }
 --- !u!1 &481244937
 GameObject:
   m_ObjectHideFlags: 0
@@ -2156,7 +2186,7 @@ MonoBehaviour:
   syncDirection: 0
   syncMode: 0
   syncInterval: 0
-  UIWaitForPlayers: {fileID: 1056838089}
+  UIWaitForPlayers: {fileID: 1056838094}
   UIPlayer: {fileID: 2005518214250875840}
   UIPostGame: {fileID: 467298983}
   UIPauseMenu: {fileID: 501671849}
@@ -2164,15 +2194,7 @@ MonoBehaviour:
   trialLoaded: 0
   openCourseUnLoaded: 0
   prefab_onlineTrialRoulette: {fileID: 7056363291119687024, guid: d8c624c72a22b2849a702fb1c7699f7d, type: 3}
-  countdown: 3
-  gameDuration: 2
-  postGameDuration: 10
-  trackEventTimeStep: 15
-  trialPool:
-  - SpeedwayRace
-  selectedTrial: RaceTrial01
   localPlayer: {fileID: 0}
-  expectedPlayersFromLobby: 0
   waitForPlayersToBeReady: 0
   countdownElapsed: 0
   gameTime: 0
@@ -2559,6 +2581,7 @@ GameObject:
   - component: {fileID: 1056838093}
   - component: {fileID: 1056838092}
   - component: {fileID: 1056838091}
+  - component: {fileID: 1056838094}
   m_Layer: 0
   m_Name: UIWaitForEveryPlayers
   m_TagString: Untagged
@@ -2649,6 +2672,26 @@ Canvas:
   m_SortingLayerID: 0
   m_SortingOrder: 0
   m_TargetDisplay: 0
+--- !u!114 &1056838094
+MonoBehaviour:
+  m_ObjectHideFlags: 0
+  m_CorrespondingSourceObject: {fileID: 0}
+  m_PrefabInstance: {fileID: 0}
+  m_PrefabAsset: {fileID: 0}
+  m_GameObject: {fileID: 1056838089}
+  m_Enabled: 1
+  m_EditorHideFlags: 0
+  m_Script: {fileID: 11500000, guid: 2c57313ae9fafa24b88761fdbb8fff47, type: 3}
+  m_Name: 
+  m_EditorClassIdentifier: 
+  _uiTheme:
+    rid: -2
+  Parent: {fileID: 0}
+  references:
+    version: 2
+    RefIds:
+    - rid: -2
+      type: {class: , ns: , asm: }
 --- !u!114 &1091680446 stripped
 MonoBehaviour:
   m_CorrespondingSourceObject: {fileID: 3150674130453725355, guid: de27dc63356a2554d8f7454a07277633, type: 3}

WONKERZ/Assets/1_Prefabs/Online/PlayerEmptySlot.prefab
@@ -16,7 +16,7 @@ GameObject:
   m_Icon: {fileID: 0}
   m_NavMeshLayer: 0
   m_StaticEditorFlags: 0
-  m_IsActive: 1
+  m_IsActive: 0
 --- !u!4 &1725610305151340587
 Transform:
   m_ObjectHideFlags: 0

WONKERZ/Assets/2_Scripts/Online/OnlinePlayerController.cs
@@ -1,16 +1,15 @@
-using Mirror;
-using Schnibble;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+
+using Mirror;
+
+using Schnibble;
+
 using Wonkerz;
 
-// Network object specs
-//
-// Client side:
-//
-// Server side:
 
 public class OnlinePlayerController : NetworkBehaviour
 {
@@ -32,27 +31,29 @@ public class OnlinePlayerController : NetworkBehaviour
 
     [Header("Online Tweaks")]
     public float minSpeedToDoDamage = 30f;
+
     [Header("Internals")]
-    // Client has authority on this,
+    // Client has "authority" on this, but server has authority on the object.
     // It will not modify it directly but send a cmd to try to modify it.
     // When the server sets it, the client listen for the change and react to it.
     [SyncVar]
     bool _isReady = false;
-    public bool isReady { get => _isReady; private set { _isReady = value; onAnyChange?.Invoke(); } }
+    public bool isReady   { get => _isReady  ; private set { _isReady   = value; onAnyChange?.Invoke(); } }
     [SyncVar]
     bool _isLoaded = false;
-    public bool isLoaded { get => _isLoaded; private set { _isLoaded = value; onAnyChange?.Invoke(); } }
+    public bool isLoaded  { get => _isLoaded ; private set { _isLoaded  = value; onAnyChange?.Invoke(); } }
     [SyncVar]
     bool _isSpawned = false;
     public bool isSpawned { get => _isSpawned; private set { _isSpawned = value; onAnyChange?.Invoke(); } }
 
+    #region duplicate of PlayerController values for online
     // TODO: Remove this hack and do a proper online object.
     [SyncVar(hook = nameof(OnPlayerStateChanged))]
     PlayerController.PlayerStates playerState;
     [SyncVar(hook = nameof(OnPlayerVehicleStateChanged))]
     PlayerController.PlayerVehicleStates playerVehicleState;
     // wheelOmega needs to be updated by the server.
-    SyncList<float> omegas = new SyncList<float>();
+    readonly SyncList<float> omegas = new SyncList<float>();
 
     public void OnUpdateOmegas(SyncList<float>.Operation op, int itemIndex, float oldItem, float newItem)
     {
@@ -95,18 +96,13 @@ public class OnlinePlayerController : NetworkBehaviour
             axle.left.SetAngularVelocity(omegas[3]);
         }
     }
+    #endregion
 
     public Transform cameraFocusable;
-
     // Will be called when anything changes on this online player.
     // To do this we removed any public variable to have getter/setter
     // that call this callback if anyone is listening.
     public Action onAnyChange;
-
-    public bool IsSpawned() => isSpawned;
-    public bool IsReady() => isReady;
-    public bool IsLoaded() => isLoaded;
-    public bool IsLockAndLoaded() => IsReady() && IsLoaded();
 
     void FixedUpdate()
     {
@@ -115,7 +111,7 @@ public class OnlinePlayerController : NetworkBehaviour
 
         if (isServer)
         {
-            if (IsLoaded())
+            if (isLoaded)
             {
                 if (self_PlayerController.IsCar())
                 {
@@ -335,7 +331,7 @@ public class OnlinePlayerController : NetworkBehaviour
     void OnStartServerInit()
     {
         // TODO: remove anything linked to input poll, rendering, etc...
-        self_PlayerController.OnStateChange += OnStateChange;
+        self_PlayerController.OnStateChange        += OnStateChange;
         self_PlayerController.OnVehicleStateChange += OnVehicleStateChange;
     }
 

WONKERZ/Assets/2_Scripts/Online/OnlineRaceTrialManager.cs
@@ -15,8 +15,8 @@ public class OnlineRaceTrialManager : OnlineTrialManager
     
     [Header("Race Trial Internals")]
     public OnlineRaceCheckPoint RootCP;
-    public SyncDictionary<OnlinePlayerController, int> dicPlayersToLaps = new SyncDictionary<OnlinePlayerController, int>();
-    public SyncDictionary<OnlinePlayerController, OnlineRaceCheckPoint> dicPlayersToCP = new SyncDictionary<OnlinePlayerController, OnlineRaceCheckPoint>();
+    public readonly SyncDictionary<OnlinePlayerController, int> dicPlayersToLaps = new SyncDictionary<OnlinePlayerController, int>();
+    public readonly SyncDictionary<OnlinePlayerController, OnlineRaceCheckPoint> dicPlayersToCP = new SyncDictionary<OnlinePlayerController, OnlineRaceCheckPoint>();
     public LinkedList<OnlineRaceCheckPoint> checkpoints = new LinkedList<OnlineRaceCheckPoint>();
 
     void Start()
@@ -48,11 +48,6 @@ public class OnlineRaceTrialManager : OnlineTrialManager
 
         // init players
         // retrieved from OGM.uniquePlayers
-        
-        // init dic players to laps
-        dicPlayersToLaps = new SyncDictionary<OnlinePlayerController, int>();
-        // init dic players to CP
-        dicPlayersToCP = new SyncDictionary<OnlinePlayerController, OnlineRaceCheckPoint>();
         foreach (OnlinePlayerController opc in OnlineGameManager.singleton.uniquePlayers)
         {
             dicPlayersToLaps.Add(opc, 1);

WONKERZ/UserSettings/EditorUserSettings.asset
@@ -27,10 +27,10 @@ EditorUserSettings:
       value: 565055550400500b0b590d2715720c444f4e48287a7a76347d2b1e65b2b0673e
       flags: 0
     RecentlyUsedSceneGuid-7:
-      value: 5057510056500d5f59590a26142759444f4e19287a7e2062747d1c30e4b56660
+      value: 5202505400075c0f595e557b4671064412161b2874782267747d1860bae56d3b
       flags: 0
     RecentlyUsedSceneGuid-8:
-      value: 5202505400075c0f595e557b4671064412161b2874782267747d1860bae56d3b
+      value: 5057510056500d5f59590a26142759444f4e19287a7e2062747d1c30e4b56660
       flags: 0
     RecentlyUsedSceneGuid-9:
       value: 5a5757560101590a5d0c0e24427b5d44434e4c7a7b7a23677f2b4565b7b5353a

commit c2a3afa50e48489970441bf375c1938fa245667e

[Bugs] Fix some bugs introduced with commit 2a67728635b42eb0570e9af80d6e69cbb9b16f63

WONKERZ/Assets/1_Prefabs/UI/UIStartTrack.prefab
@@ -258,16 +258,16 @@ MonoBehaviour:
     m_Curve:
     - serializedVersion: 3
       time: 0
-      value: 0.4988098
-      inSlope: 2
-      outSlope: 2
+      value: 1
+      inSlope: 0
+      outSlope: 0
       tangentMode: 0
       weightedMode: 0
       inWeight: 0
       outWeight: 0
     - serializedVersion: 3
       time: 1
-      value: 1
+      value: 0.5
       inSlope: 0
       outSlope: 0
       tangentMode: 0

WONKERZ/Assets/2_Scripts/Online/OnlineCollectible.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 
 using Mirror;
+
+using Schnibble;
 
 public enum ONLINE_COLLECTIBLES {
     NONE=0,
@@ -60,7 +63,7 @@ public class OnlineCollectible : NetworkBehaviour
         }
     }
 
-    
+
 
     [Server]
     public void SetAsNegative()
@@ -77,7 +80,7 @@ public class OnlineCollectible : NetworkBehaviour
 
         value = -1;
         RpcChangeImageToNegative();
-            
+
         Image img = GetComponentInChildren<Image>();
         if (!!img)
         {
@@ -109,13 +112,17 @@ public class OnlineCollectible : NetworkBehaviour
         if (!!AC)
         {
             OnlinePlayerController locPlayer = AC.attachedOnlinePlayer;
+            if (locPlayer == null) {
+                this.LogError("locPlayer is null.");
+            }
+
 
             if (locPlayer.bag.HasAPowerEquipped && IsPowerCollectible())
                 return; // player already has a power we don't collect
 
             if (iCollider.transform.IsChildOf(locPlayer.transform))
                 OnCollect(locPlayer);
-            
+
             DestroySelf();
         }
     }

WONKERZ/Assets/2_Scripts/Online/OnlineGameManager.cs
@@ -24,7 +24,7 @@ using Wonkerz;
 //
 // Client side:
 //    - React to change of SyncVars and react accordingly.
-//    - Set the spawnde/loaded/ready state on the server. 
+//    - Set the spawnde/loaded/ready state on the server.
 //
 // Server side:
 //    - keep track of connected player with a SyncList<OnlinePlayerController> uniquePlayers
@@ -91,6 +91,7 @@ public class OnlineGameManager : NetworkBehaviour
     [Header("References")]
     public GameObject prefab_onlineTrialRoulette;
 
+    [System.Serializable]
     public class GameSettings
     {
         public uint countdownDuration  = 3; // in seconds
@@ -352,7 +353,7 @@ public class OnlineGameManager : NetworkBehaviour
 
         FreezeAllPlayers(false);
 
-        gameTime = settings.gameDuration;
+        gameTime     = settings.gameDuration;
         gameLaunched = true;
     }
 

WONKERZ/Assets/2_Scripts/Online/OnlineStartLine.cs
@@ -28,7 +28,7 @@ public class OnlineStartLine : NetworkBehaviour, IControllable
 {
     [Header("MAND")]
     //public string track_name;
-    
+
     public GameObject UIStartTrackRef;
     public AudioSource  startLineCrossed_SFX;
     public GameObject meshModelHandle;
@@ -112,16 +112,18 @@ public class OnlineStartLine : NetworkBehaviour, IControllable
         int lastTime = 0;
         while (OGM.countdownElapsed > 0.0f)
         {
-            // check if we passed a second, from 3 to 2 for instance.
+            // check if we pass a second, from 3 to 2 for instance.
             int  currentTime   = (int)OGM.countdownElapsed;
             bool passedASecond = lastTime != currentTime;
+            lastTime = currentTime;
+
             if (passedASecond) {
                 audioSource.clip = countDownSFX0;
                 audioSource.Play(0);
-            } 
+            }
 
-            UIStartTrackInst.updateDisplay(OGM.countdownElapsed, !passedASecond);
-            
+            UIStartTrackInst.updateDisplay(OGM.countdownElapsed, passedASecond);
+
             yield return null;
         }
         // Play sound at the end.
@@ -129,7 +131,7 @@ public class OnlineStartLine : NetworkBehaviour, IControllable
         audioSource.Play(0);
         // Make sure we show 0
         UIStartTrackInst.updateDisplay(0);
-        
+
         launchTrack();
     }
 
@@ -176,7 +178,7 @@ public class OnlineStartLine : NetworkBehaviour, IControllable
         meshModelHandle.gameObject.SetActive(false);
 
         yield return new WaitForSeconds(2f);
-        
+
         Destroy(UIStartTrackInst.gameObject);
 
         OnlineGameManager.singleton.startLine = null;

WONKERZ/Assets/2_Scripts/Online/OnlineUIPauseMenu.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.Events;
@@ -18,7 +18,7 @@ using Wonkerz;
 
 // TODO : Induces input buffering (ex start jump, pause, spam jump, unpause => boom rocket jump)
 // THUS !! Player must be frozen and most likely any kind of User inputs beside this pause menu.
-public class OnlineUIPauseMenu : UIControllableElement 
+public class OnlineUIPauseMenu : UIControllableElement
 {
     public GameObject UIHandle;
     public UIControllableElement panel;
@@ -41,11 +41,11 @@ public class OnlineUIPauseMenu : UIControllableElement
             attachedPlayer = Access.Player();
             if (attachedPlayer == null) {
                 this.LogError("No player has been attached.");
+                return;
             }
         }
 
         attachedPlayer.inputMgr.Attach(this);
-
 
         if (NetworkClient.activeHost) {
             var tabs = (panel as UIPanelTabbed).tabs;

WONKERZ/Assets/2_Scripts/Online/UIRoom.cs
@@ -46,7 +46,6 @@ public class UIRoom : UIPanelTabbed
     // string based for now.
     static readonly string startHint = "Press ";
     static readonly string readyUpHintEnd = " to ready up.";
-
     static readonly string readyUpButton = "UIStart";
 
     public UILabel   readyUpHint;
@@ -135,6 +134,7 @@ public class UIRoom : UIPanelTabbed
             uiOnline.SetState(UIOnline.States.Deactivated);
         }
     }
+
 
     public void OnAllPlayersReady()
     {

WONKERZ/Assets/2_Scripts/UI/UIStartTrack.cs
@@ -1,21 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+using Schnibble;
 
 public class UIStartTrack : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI countdown_txt;
 
     // 1D alpha texture.
+    // NOTE: must be One dimensionnal byte type.
     public Texture2D opacityRamp;
 
     public AnimationCurve fontSizeRatioOverTime;
 
-    private float initFontSize = 12f;
+    float initFontSize = 12f;
 
-    void Start()
+    void OnValidate() {
+        if (opacityRamp.height != 1) {
+            this.LogError("opacityRamp should be a 1D texture, height is " + opacityRamp.height);
+        }
+
+        if (opacityRamp.format != TextureFormat.R8) {
+            this.LogError("opacityRamp should be a single channel byte texture.");
+        }
+
+        if (!opacityRamp.isReadable) {
+            this.LogError("opacityRamp should be readable.");
+        }
+    }
+
+    void OnEnable()
     {
         initFontSize = countdown_txt.fontSize;
+    }
+
+    void OnDisable() {
+        countdown_txt.fontSize = initFontSize;
+        countdown_txt.alpha    = 1.0f;
     }
 
     // Must be called from outside.
@@ -24,22 +44,15 @@ public class UIStartTrack : MonoBehaviour
     {
         if (updateString)
         {
-            if (iCurrCountdownValue == 0.0f)
-            {
-                countdown_txt.text = "GO!";
-            }
-            else
-            {
-                countdown_txt.text = Mathf.CeilToInt(iCurrCountdownValue).ToString();
-            }
+            if (iCurrCountdownValue == 0.0f) countdown_txt.text = "GO!";
+            else                             countdown_txt.text = Mathf.CeilToInt(iCurrCountdownValue).ToString();
         }
 
         float normalizedTime = iCurrCountdownValue % 1.0f;
 
-        countdown_txt.fontSize = fontSizeRatioOverTime.Evaluate(normalizedTime) * initFontSize;
+        countdown_txt.fontSize = initFontSize * fontSizeRatioOverTime.Evaluate(normalizedTime);
 
-        float newAlpha = opacityRamp.GetPixelData<float>(0)[(int)(normalizedTime * opacityRamp.width)];
-        countdown_txt.alpha = newAlpha;
-
+        byte newAlpha       = opacityRamp.GetPixelData<byte>(0)[(int)(normalizedTime * opacityRamp.width)];
+        countdown_txt.alpha = (float)newAlpha / 255.0f;
     }
 }